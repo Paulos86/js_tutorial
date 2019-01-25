999432b0e111ee5ef208ac33f4ed99a2e2ae5ac7
Paul Badenhorst <paul@catostylus.com>
2019-01-25 18:34:21 +0200

    remove jank

--- day.js
+++
@@ -1,9 +0,0 @@
-function dayName(date){
-  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday',
-                      'Thursday', 'Friday', 'Saturday']
-  return daysOfWeek[now.getDay()]
-}
-
-function greeting(date){
-  return `Hello, world! Happy ${dayName(date)}.`
-}
--- foreach.js
+++
@@ -1,4 +0,0 @@
-let a = ["ant", "bat", "cat", 42];
-a.forEach((element) => {
-  console.log(element);
-});
